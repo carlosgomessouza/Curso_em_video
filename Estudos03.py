ddecb31a252b84c5277f6be187a59e8c50cab75f
kadu <kaduce2009@hotmail.com>
2019-05-04 23:19:29 -0300

    Começo do python

---
+++ Estudos03.py
@@ -0,0 +1,4 @@
+dia = input ('Qual dia que nasceu?')
+mes = input (' Qual mes que nasceu?')
+ano = input (' Qual ano em que nasceu?')
+print ('Voce nasceu no dia', dia, "de", mes, "de", ano, '.', 'Correto?')
---
+++ Estudos04.py
@@ -0,0 +1,6 @@
+x = int (input('ditie um numero'))
+
+y = int (input ('digite um numero que queira somar com o primeiro'))
+
+z = x+y
+print (z)
